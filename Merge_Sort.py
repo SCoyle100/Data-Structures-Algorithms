87d393508046abcf6170d5365c70235a72517341
Slaestro <83990984+Slaestro@users.noreply.github.com>
2021-12-15 17:12:18 +0000

    Added a script called Merge_Sort.py

---
+++ Merge_Sort.py
@@ -0,0 +1,17 @@
+//Merge sort would break this list (5,4,7,1,3,2,8,6) down again and again until the only thing that is left is a list with only 1 item in them.  So 8 separate lists with 1 item in each one.  A list with 1 item in it, is by definition, "sorted".
+
+
+//Merge function
+
+def merge(list1, list2)
+  //new list to combine list1 and list2
+  combined = []
+    i = 0
+    j = 0
+      while i < len(list1) and j < len(list2):
+        if list[i] < list2[j]:
+          combined.append(list1[i])
+          i += 1
+        else;
+          combined.append(list2[j])
+          j += 1
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Data-Structures-Algorithms
+//Doubly Linked List Practice
+
+class DoublyLinkedList:
+  def__init__(self, value):
+    new_node = Node(value)
+    self.head = new_node
+    self.tail = new_node
+    self.length = 1
+
+
+    my_doubly_linked_list = DoublyLinkedList(7)
