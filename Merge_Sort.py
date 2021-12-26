8e84c1c8391787a6e2a760b55e91dbddd5344e79
SCoyle100 <83990984+SCoyle100@users.noreply.github.com>
2021-12-26 19:05:18 +0000

    added text

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -1,10 +1,12 @@
-//Merge sort would break this list (5,4,7,1,3,2,8,6) down again and again until the only thing that is left is a list with only 1 item in them.  So 8 separate lists with 1 item in each one.  A list with 1 item in it, is by definition, "sorted".
+# Merge sort would break this list (5,4,7,1,3,2,8,6) down again and again until the only thing that is left is a list with only 1 item in them.  So 8 separate lists with 1 item in each one.  A list with 1 item in it, is by definition, "sorted".
 
 
-//Merge function
+# Merge function
+
+# adding text for Github testing
 
 def merge(list1, list2)
-  //new list to combine list1 and list2
+  # new list to combine list1 and list2
   combined = []
     i = 0
     j = 0
---
+++ Testing.py
@@ -0,0 +1,5 @@
+# Testing
+
+print ("hello world");
+
+return
