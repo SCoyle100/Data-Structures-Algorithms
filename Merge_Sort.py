089b86294dafe0448da0aa36641fec8fd05b4426
SCoyle100 <83990984+SCoyle100@users.noreply.github.com>
2022-04-05 13:13:47 -0400

    Adding natural language translation
    Added notes next to each line of code to translate from Python to natural language using OpenAI.

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -1,7 +1,6 @@
 # Merge sort would break this list (5,4,7,1,3,2,8,6) down again and again until the only thing that is left is a list with only 1 item in them.  So 8 separate lists with 1 item in each one.  A list with 1 item in it, is by definition, "sorted".
 
 
-# Merge function
 
 # Notes next to the lines of code are from OpenAI - translating Python to natural language
 
