2a6863339e414f0f0850dc3751fc476e016a90a3
SCoyle100 <83990984+SCoyle100@users.noreply.github.com>
2022-04-05 13:12:02 -0400

    Update Merge_Sort.py

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -3,17 +3,20 @@
 
 # Merge function
 
-# adding text for Github testing
+# Notes next to the lines of code are from OpenAI - translating Python to natural language
 
 def merge(list1, list2)
-  # new list to combine list1 and list2
-  combined = []
-    i = 0
+  
+  combined = [] #create an empty list to store the combined lists
+    i = 0 # create two variables, i and j, to keep track of the index of list1 and list2
     j = 0
-      while i < len(list1) and j < len(list2):
-        if list[i] < list2[j]:
-          combined.append(list1[i])
+      while i < len(list1) and j < len(list2): # while the index of list1 is less than the length of list1 and the index of list2 is less than the length of list2:
+        if list[i] < list2[j]: # if the item at index i of list1 is less than the item of index j of list2:
+          combined.append(list1[i]) # add the item at index i of list1 to the empty list and increment i by 1
           i += 1
-        else;
-          combined.append(list2[j])
+        else; # otherwise:
+          combined.append(list2[j]) # add the item at index j of list2 to the empty list and increment j by 1
           j += 1
+          
+          # while the index of list1 is less than the length of list1:
+          # add the item at index i of list1 to the empty list and increment i by 1
